33df356f5c5df3139eab1a1c8dcee83b696f0267
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-03-12 22:08:41 +0200

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,5 +8,14 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
 }
+
 module.exports = Rectangle;
