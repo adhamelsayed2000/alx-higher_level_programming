e8794ccb0042f91aaeff47906e2f1e155c57df36
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-03-27 15:06:22 +0200

    edite commint in file 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
+# send a request to an URL with curl
 curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
