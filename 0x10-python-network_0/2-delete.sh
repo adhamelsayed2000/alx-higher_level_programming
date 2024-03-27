2f2e050bd60afe83cee0491172865f21f4dc7a95
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-03-27 15:18:43 +0200

    task 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+#DELETE request to the URL passed as the first argument and displays
+curl -sX DELETE "$1"
