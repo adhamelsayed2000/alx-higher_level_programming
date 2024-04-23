e5d9d6970a57989759edbfb772240d3c123f8f6a
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-04-23 20:48:01 +0200

    add smic

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results
@@ -7,6 +7,6 @@ request(process.argv[2], function (error, response, body) {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count
-    }, 0))
+    }, 0));
   }
-})
+});
