ea8d2360a50e5df79443455b2c7144d1b39d05dd
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-04-23 14:41:38 +0200

    fixed wc

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
+
 const fs = require('fs')
 const file = process.argv[2]
-
-function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(data)
-    }
-  })
-}
-readPrint(file)
+fs.readFile(file, 'utf-8', function (err, data = err) {
+  console.log(data)
+})
